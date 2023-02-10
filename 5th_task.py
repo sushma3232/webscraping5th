e5dfb6e0254f37b4501b5a8ae0801d5893004f48
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-02-10 11:27:01 +0530

    Add files via upload

---
+++ 5th_task.py
@@ -0,0 +1,39 @@
+import requests
+import json
+from bs4 import BeautifulSoup
+url_list=[]
+with open('second_task.json','r') as f:
+    a=json.load(f)
+   
+for i in a:
+    url_list.append(i['url'])
+
+b=url_list[:25]
+
+details=['name','director','image','description','language','genre',]
+# print(b)
+list=[]
+for j in url_list:
+   
+    rel=requests.get(j)
+    soup=BeautifulSoup(rel.content,"html.parser")
+    con=soup.find('script',type='application/ld+json').text
+    h=json.loads(con)
+    dic={}
+    for k in h:
+        dic['name']=h['name']
+        dic['director']=h['director'][0]['name']
+        dic['image']=h['image']
+        dic['description']=h['description']
+        dic["language"]=h['review']['inLanguage']
+        dic['genre']=h['genre']
+        dic['country']='india'
+    list.append(dic)
+
+with open('5th_task.json','w') as file:
+    json.dump(list,file,indent=8)
+
+
+
+
+
